3786c5c3b76552e1a771f0763831d6df6090180b
Shunyakatada <85655151+shunyasaitama@users.noreply.github.com>
2023-03-19 16:04:45 +0900

    feat: ace-window

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -45,7 +45,8 @@
   (tool-bar-mode -1))
 
 (use-package ivy
-  :bind (("C-s" . swiper))
+  :bind
+  (("C-s" . swiper))
   :config
   (ivy-mode t))
 
@@ -58,7 +59,9 @@
   :ensure t)
 
 (use-package counsel
-  :ensure t)
+  :ensure t
+  :bind
+  (("C-x C-b" . counsel-switch-buffer)))
 
 (use-package company
   :ensure t
@@ -66,7 +69,10 @@
   (company-minimum-prefix-length 1)
   (company-idle-delay 0.0)
   :config
-  (global-company-mode t))
+  (global-company-mode t)
+  :bind
+  (:map company-active-map
+	("C-h" . 'backward-delete-char)))
 
 (use-package company-quickhelp
   :after company
@@ -77,4 +83,19 @@
 (use-package company-box
   :ensure t
   :hook (company-mode . company-box-mode))
+
+(use-package ace-window
+  :ensure t
+  :bind (("C-x C-o" . ace-window))
+  :init
+  (setq aw-dispatch-always nil)
+  (setq aw-dispatch-alist
+        '((?x aw-delete-window " Ace - Delete Window")
+          (?m delete-other-windows " Ace - Delete Other Windows")
+          (?b balance-windows " Ace - Balance Windows")
+          (?s ace-swap-window " Ace - Swap Window")
+          (?n aw-flip-window)
+          (?i aw-swap-iw " Ace - Swap with Ace-Window")
+          (?o delete-other-windows)
+          (?? aw-show-dispatch-help))))
 
